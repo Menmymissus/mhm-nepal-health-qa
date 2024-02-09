714d2e3fa7acfbd2d0387b696f7c7e93361f0b26
Menmymissus <legendkiller11918114@gmail.com>
2024-02-09 12:58:55 +0545

    added dashboard

--- mhmfrontendframework/src/components/Dashboard.jsx
+++ mhmfrontendframework/src/components/Dashboard.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import {useState, useEffect} from 'react';
 
 const Dashboard = () => {
+  const[generatedTexts, setGeneratedTexts]=useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('http://localhost:8000/api/dashboard/get-generated-texts/');
+        const data = await response.json();
+        setGeneratedTexts(data.generated_texts);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+    fetchData();
+  }, []);
+
+
   return (
-    <div>This is dashboard from log in</div>
+    <main className=''>
+    <div className='' style={{ maxHeight: '500px', overflowY: 'auto' }}>
+      <h1 className='text-2xl font-bold font-spaceGrotesk'>History</h1>
+      {generatedTexts.map((generatedText, index) => (
+        <div key={index}>
+          <strong>Query:</strong> {generatedText.input_text}<br />
+          <strong>Answer:</strong> {generatedText.generated_text}<br />
+          <strong>Created At:</strong> {generatedText.created_at}<br />
+          <hr />
+        </div>
+      ))}
+    </div>
+    <div className=''>
+      Hello
+    </div>
+    </main>
   )
 }
 
-export default Dashboard
+export default Dashboard;
