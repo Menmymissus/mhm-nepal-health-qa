ab54dd13bf127ce05fc28dc526adb783561e0f29
Menmymissus <legendkiller11918114@gmail.com>
2024-02-15 08:26:51 +0545

    About us ko

---
+++ mhmfrontendframework/src/components/Aboutprops.jsx
@@ -0,0 +1,11 @@
+
+
+function Aboutprops({title}) {
+  return (
+    <div className='h-[30vh] w-[40vh] border-2 rounded sm items-center justify-center flex backdrop-blur-sm'>
+     <div>{title}</div>
+    </div>
+  )
+}
+
+export default Aboutprops;
